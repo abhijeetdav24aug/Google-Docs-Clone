a762efb7fc5491130a3f4230ae055357a01d85ac
Abhijeet Gaurav <65087356+abhijeetdav24aug@users.noreply.github.com>
2023-05-23 19:30:44 +0530

    firebaseconfig updated

--- src/fireabase/config.js
+++ src/fireabase/config.js
@@ -5,12 +5,13 @@ import { getAuth } from "@firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFKkpY6J5inB9eI8gSFrgZxFp_AnTH9oM",
-  authDomain: "docs-d125f.firebaseapp.com",
-  projectId: "docs-d125f",
-  storageBucket: "docs-d125f.appspot.com",
-  messagingSenderId: "760604561008",
-  appId: "1:760604561008:web:21df79493c1658cc8d6eb5",
+  apiKey: "AIzaSyDmZyogTk_zL0yMM7x3e_88jmtJhm7pY8M",
+  authDomain: "docs-clone-2f99b.firebaseapp.com",
+  projectId: "docs-clone-2f99b",
+  storageBucket: "docs-clone-2f99b.appspot.com",
+  messagingSenderId: "994818944830",
+  appId: "1:994818944830:web:e3e7c18d5ded5af464866f",
+  measurementId: "G-93PRK09N67"
 };
 
 // Initialize Firebase
